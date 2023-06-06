049b14591a90b1da4ed8782c174a40dc26738b52
dopabean26 <130524460+dopabean26@users.noreply.github.com>
2023-06-05 22:36:03 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 window.onload = function() {
-    fetch('https://www.ea.com/games/apex-legends/news#game-updates')
+    fetch('https://raw.githubusercontent.com/dopabean26/my_new_project/main/patch_notes.txt')
         .then(response => response.text())
-         .then(data => {
-            let parser = new DOMParser();
-            let doc = parser.parseFromString(data, 'text/html');
-            let updates = doc.querySelector('#game-updates');
-            document.getElementById('updates').innerHTML = updates.innerHTML;
+        .then(data => {
+            let updates = data.split('\n\n').map(update => {
+                let [title, ...notes] = update.split('\n');
+                return {title, notes};
+            });
+            let updatesDiv = document.getElementById('updates');
+updates.forEach(update => {
+                let updateDiv = document.createElement('div');
+                updateDiv.className = 'update';
+                let titleDiv = document.createElement('div');
+                titleDiv.className = 'title';
+                titleDiv.textContent = update.title;
+                titleDiv.onclick = function() {
+                    this.nextElementSibling.classList.toggle('hidden');
+                };
+                let notesDiv = document.createElement('div');
+                notesDiv.className = 'notes hidden';
+                notesDiv.textContent = update.notes.join('\n');
+                updateDiv.appendChild(titleDiv);
+                updateDiv.appendChild(notesDiv);
+                updatesDiv.appendChild(updateDiv);
+            });
         })
         .catch(error => console.error('Error:', error));
 }
